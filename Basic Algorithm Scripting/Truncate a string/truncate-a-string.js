47bb7bd5d29162af98520e8acd7446349005e33d
Brian Tran <briandinhtran@outlook.com>
2017-08-15 17:58:04 -0700

    completed truncate a string

---
+++ Basic Algorithm Scripting/Truncate a string/truncate-a-string.js
@@ -0,0 +1,10 @@
+function truncateString(str, num) {
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+
+}
